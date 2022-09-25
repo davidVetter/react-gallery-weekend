8c3ca820a7f584d7381df5551a5f49f76000bbae
David Vetter <ddvetter23@gmail.com>
2022-09-25 05:55:44 -0500

    added some base styling to imgs and descriptions

--- src/components/GalleryItem/GalleryItem.js
+++ src/components/GalleryItem/GalleryItem.js
@@ -1,7 +1,9 @@
 import Axios from "axios";
+import {useState, useEffect} from 'react';
 
 function GalleryItem(props) {
-    
+    const [toggleDescription, setToggleDescription] = useState(false);
+
     function addLike(id) {
         Axios({
             method: "PUT",
@@ -12,14 +14,32 @@ function GalleryItem(props) {
             console.log('Error in like PUT: ', error);
         });
     }
-    
+
+    function toggle() {
+       setToggleDescription(!toggleDescription);
+    }
+
+    if (!toggleDescription) {
     return (
-        <div key={props.item.id} className="imgDiv">
-            <img key={props.item.id} src={props.item.path}></img><br />
+        <div className="imgDiv">
+            <div key={props.item.id} onClick={() => {toggle()}}>
+                <img key={props.item.id} src={props.item.path}></img><br />
+            </div>
             <button onClick={() => {addLike(props.item.id)}}>Like</button>
             <p>{props.item.likes}</p>
         </div>
     )
+    } else {
+        return (
+            <div className="imgDiv">
+                <div key={props.item.id} onClick={() => {toggle()}} className="descriptionDiv">
+                    <p>{props.item.description}</p><br />
+                </div>
+                <button onClick={() => {addLike(props.item.id)}}>Like</button>
+                <p>{props.item.likes}</p>
+            </div>
+        )
+    }
 }
 
 export default GalleryItem;
