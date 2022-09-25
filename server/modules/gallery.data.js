d380e92d8f74ade08f526831faa39692537f2784
David Vetter <ddvetter23@gmail.com>
2022-09-25 07:28:54 -0500

    connected database, updated GET and PUT to reference react_gallery database

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,9 +1,9 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', title: 'goat', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
-    { id: 2, path: 'images/goat_small.jpg', title: 'goat', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
-    { id: 3, path: 'images/goat_small.jpg', title: 'goat', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
-    { id: 4, path: 'images/goat_small.jpg', title: 'goat', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
-    { id: 5, path: 'images/goat_small.jpg', title: 'goat', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
+    { id: 1, path: 'https://live.staticflickr.com/65535/52381758482_4a5afa76c4_h.jpg', title: 'goat', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
+    { id: 2, path: 'https://images.unsplash.com/photo-1558980394-4c7c9299fe96?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80', title: 'Harley Davidson', description: 'Harley Davidson', likes: 0 },
+    { id: 3, path: 'https://images.unsplash.com/photo-1597872200969-2b65d56bd16b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80', title: 'Gaming Computer', description: 'Gaming Computer', likes: 0 },
+    { id: 4, path: 'https://images.unsplash.com/photo-1548199973-03cce0bbc87b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80', title: 'Dogs running', description: 'Dogs Running', likes: 0 },
+    { id: 5, path: 'https://images.unsplash.com/photo-1532015917327-c7c46aa1d930?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1453&q=80', title: 'A man fishing in a stream', description: 'A man fishing in a stream', likes: 0 },
 ];
 
 module.exports = galleryItems;
